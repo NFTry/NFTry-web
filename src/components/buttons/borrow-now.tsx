148924a9d08db4edc6e468dfd88b214ca075d547
Imchaemin <chaaaamni@gmail.com>
2023-06-04 19:19:56 +0900

    fix borrowable card button width

--- src/components/buttons/borrow-now.tsx
+++ src/components/buttons/borrow-now.tsx
@@ -9,7 +9,7 @@ export const BorrowNowButton = ({ text, ...rest }: Props) => {
 };
 
 const Wrapper = tw.button`
-  flex-center flex-shrink-0 px-16 rounded-4 bg-black h-60
+  flex-center flex-shrink-0 px-16 rounded-4 bg-black h-60 w-145
   font-semibold text-16 leading-20 text-white clickable
 
   hover:bg-black/70
